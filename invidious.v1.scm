8739cf6028381f4ef1afa15b1d60ba03213d6fa5
siiky <siiky@users.noreply.github.com>
2019-07-27 13:55:08 +0100

    Use URI instead of strings
    `uri->string` can still be used to obtain the string representation

--- invidious.v1.scm
+++ invidious.v1.scm
@@ -2,8 +2,9 @@
   invidious.v1
   (
    *fields*
-   *instance*
+   *host*
    *pretty?*
+   *scheme*
 
    annotations
    captions
@@ -34,7 +35,8 @@
           make-parameter
           sub1)
     (only chicken.string
-          ->string)
+          ->string
+          string-split)
     chicken.syntax
     chicken.type)
 
@@ -45,16 +47,16 @@
     (only srfi-13
           string-join)
     (only uri-common
-          form-urlencode))
+          make-uri))
 
   ;;;
   ;;; General & Utility functions
   ;;;
 
-  ;; @brief Transform a list of fields into a parameter list, ready to be
-  ;;        passed to form-urlencode
+  ;; @brief Transform a list of fields into a parameter list, to
+  ;;        make an URI
   ;; @param fields A list of fields
-  ;; @returns A parameter list, ready to be passed to form-urlencode
+  ;; @returns A parameter list, to make an URI
   (define (fields-parm fields)
     (if (null? fields)
         '()
@@ -70,17 +72,16 @@
       "`*fields*` must be `#f` or a (posibly empty) list of strings or symbols")
     (or fields '()))
 
-  ;; @brief Transforms an optional field list into a parameter list, ready to
-  ;;        be passed to form-urlencode
+  ;; @brief Transforms an optional field list into a parameter list,
+  ;;        make an URI
   ;; @param fields #f or a list of fields
-  ;; @returns A parameter list, ready to be passed to form-urlencode
+  ;; @returns A parameter list, to make an URI
   (define (optional-fields-parm fields)
     (fields-parm (sanitize-optional-fields-parm fields)))
 
-  ;; @brief Transform pretty? into a parameter list, ready to be passed to
-  ;;        form-urlencode
+  ;; @brief Transform pretty? into a parameter list, make an URI
   ;; @param pretty? #f or non-#f
-  ;; @returns The singleton list with the '(pretty . 1) pair, or the epty list
+  ;; @returns '((pretty . 1)) or '()
   (define (pretty?-parm pretty?)
     (if pretty? '((pretty . 1)) '()))
 
@@ -91,66 +92,81 @@
   (define (filter-parms parms)
     (filter cdr parms))
 
-  ;; @brief Appends all parameter lists into a single parameter list, ready to
-  ;;        be passed to form-urlencode
+  ;; @brief Appends all parameter lists into a single parameter list,
+  ;;        to make an URI
   ;; @param parms Other parameters
   ;; @param fields-optional The list of fields
   ;; @param pretty? The pretty? flag
-  ;; @returns A parameter list, ready to be passed to form-urlencode
+  ;; @returns A parameter list, to make an URI
   (define (combine-parms parms fields-optional pretty?)
     (let ((fields-parm (optional-fields-parm fields-optional))
           (pretty?-parm (pretty?-parm pretty?)))
       (append pretty?-parm fields-parm parms)))
 
+  (define (method-symbol->path sym)
+    (string-split (symbol->string sym) "/"))
+
+  (define (positional->path positional)
+    (map ->string  positional))
+
+  (define (path method positional)
+    `(/ "api" "v1" ,@method ,@positional))
+
   ;; @brief Makes a complete query URL
-  ;; @param base The base request URL, made by concatenating the instance URL,
-  ;; the API path and the command
-  ;; @param parms The command specific parameters
+  ;; @param method-sym The method-sym request URL, made by concatenating
+  ;;     the instance URL, the API path and the method
+  ;; @param parms The method specific parameters
   ;; @param fields The list of fields
   ;; @param pretty? The pretty? flag
   ;; @returns A complete query URL
-  (define (make-query-url base parms fields pretty?)
-    (let ((parms (combine-parms parms fields pretty?)))
-      (string-append base "?" (or (form-urlencode parms) ""))))
-
-  ;; @brief Makes the base API URL, optionally with a command
-  ;; @param command The command
-  ;; @returns The base API URL
-  (define (api-url #!optional (command "") (id/ucid ""))
-    ; FIXME: command="" & id/ucid="" => "<instance>/api/v1//"
-    (string-append (*instance*) "api/v1/" command "/" (->string id/ucid)))
+  (define (make-query-url method-sym parms fields pretty? positional)
+    (make-uri #:scheme (*scheme*)
+              #:path (path (method-symbol->path method-sym)
+                           (positional->path positional))
+              #:query (combine-parms parms fields pretty?)
+              #:host (*host*)))
 
   ;;;
   ;;; Configuration parameters
   ;;;
 
-  ;; @brief Pretty-print JSON response?
+  ;; @brief Wrapper for chicken.base.assert
+  ;; @param type The expected type
+  ;; @param type? Predicate for @a type
+  ;; @param var The variable this assert is protecting
+  (define (assert* type type? var)
+    (lambda (val)
+      (assert (type? val) (string-append "`" var "` must be a " type))
+      val))
+
+  ;; @brief Pretty-print JSON response? (default is #f)
   ;; @see https://github.com/omarroth/invidious/wiki/API#pretty
   (define *pretty?* (make-parameter #f (compose not not)))
 
-  ;; @brief The fields of the response one is interested in
+  ;; @brief The fields of the response one is interested in (default is '())
   ;; @see https://github.com/omarroth/invidious/wiki/API#fields
   ;; @see https://developers.google.com/youtube/v3/getting-started#fields
   (define *fields* (make-parameter '() sanitize-optional-fields-parm))
 
-  ;; @brief The instance to use
+  ;; @brief The scheme to be used (HTTP/S) (default is HTTPS)
+  ;; @see uri-common
+  (define *scheme* (make-parameter 'https (assert* "symbol" symbol? "*scheme*")))
+
+  ;; @brief The host of the instance to use (default is invidio.us)
   ;; @see https://github.com/omarroth/invidious/wiki/Invidious-Instances
-  (define *instance*
-    (make-parameter
-      "https://invidio.us/"
-      (lambda (str)
-        (assert (string? str) "`*instance*` must be a string")
-        (if (char=? #\/ (string-ref str (sub1 (string-length str))))
-            str
-            (string-append str "/")))))
+  (define *host* (make-parameter "invidio.us" (assert* "string" string? "*host*")))
 
   ;;;
-  ;;; Command functions
+  ;;; Method functions
   ;;;
-  ;;; Command functions accept the key `fields` and `pretty?` arguments
+  ;;; All method functions, in addition to the method specific parameters
+  ;;;     specified in the Invidious API documentation, accept the key
+  ;;;     `fields` and `pretty?` arguments,
+  ;;;
   ;;; The `fields` key argument can be any value. #f disables the fields
   ;;;     parameter; if given a list, it is used as the list of fields;
   ;;;     defaults to the `*fields*` parameter in all the other cases
+  ;;;
   ;;; The `pretty?` key argument enables/disables pretty printing of the
   ;;;     JSON response; defaults to the `*pretty?*` parameter, if not given
   ;;;
@@ -172,11 +188,11 @@
     (syntax-rules ()
       ((define-iv (name pos1 pos2 too-many ...) key ...)
        (syntax-error "There must be at most one positional argument"))
+
       ((define-iv (name positional ...) key ...)
        (define (name positional ... #!key (fields (*fields*)) (pretty? (*pretty?*)) (key #f) ...)
-         (let ((base (api-url (symbol->string 'name) positional ...))
-               (parms (filter-parms `((key . ,key) ...))))
-           (make-query-url base parms fields pretty?))))))
+         (let ((parms (filter-parms `((key . ,key) ...))))
+           (make-query-url 'name parms fields pretty? '(positional ...)))))))
 
   ;; @see https://github.com/omarroth/invidious/wiki/API#get-apiv1stats
   (define-iv (stats))
